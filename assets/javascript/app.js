905dd74e5ed628f4285a3a04a6d840dcf2474d8a
Kathy Kennedy <nomamanomama@yahoo.com>
2018-02-23 15:23:11 -0600

    basic working version

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,52 +12,71 @@ var gameState = ["start","question","answer"];
 
 var currentGameState = gameState[0];
 
+var game;
+
+var answerTimeout = 7000;
+
 
 
 /////////////////////////////////////////////
 //  OBJECTS 
 //
-function Question(){
-    id=0;
-    question:"";
-    answers:[];
-    answerIndex:-1;
-    rewardSrc:"";
+
+/////////////////////////////////////////
+// QUESTION OBJECTS
+function Question(id,question,answers,answerIndex,rewardSrc){
+    this.id=id;
+    this.question=question;
+    this.answers=answers;
+    this.answerIndex=answerIndex;
+    this.rewardSrc=rewardSrc;
 };
 
-var q1 = new Question(), 
-    id = 1,
-    question = "Who won the gold medal for Snowboarding in the 2014 Olympics?",
-    answers = ["Mary Lou Retton", "Shaun White", "", ""];
-answerIndex = 1;
-rewardSrc = "#";
-var q2 = new Question(),
-    id = 2,
-    question = "What did little bo peep lose?",
-    answers = ["goat", "kitten", "dog", "sheep"];
-answerIndex = 3;
-rewardSrc = "#";
-var q3 = new Question(),
-    id = 3,
-    question = "What is the largest state in the US?",
-    answers = ["Texas", "California", "Alaska", "Rhode Island"];
-answerIndex = 2;
-rewardSrc = "#";
-
+var q1 = new Question(
+    1,
+    "Who won the gold medal for Snowboarding in the 2014 Olympics?",
+    ["Mary Lou Retton", "Shaun White", "Bo Jangles", "Tom Skerrit"],
+    1,
+    "#"
+);
+var q2 = new Question(
+    2,
+    "What did little bo peep lose?",
+    ["goat", "kitten", "dog", "sheep"],
+    3,
+    "#"
+);    
+var q3 = new Question(
+    3,
+    "What is the largest state in the US?",
+    ["Texas", "California", "Alaska", "Rhode Island"],
+    2,
+    "#"
+);
 
 var questionArray = [q1, q2, q3];
 
-function Timer () {
+
+/////////////////////////////////////////
+// TIMER OBJECT
+
+function Timer (divTag) {
     this.time=30,
+    this.divTag = divTag;
+    this.intervalID=0;
     this.count = function () {
         time--;
-        if (time === 0) this.stop();
+        $(this.divTag).text(time);
+        if (time === 0) { //the time has run out with no answer
+           this.stop(); 
+           $(this.divTag).dispatchEvent("change");
+        } 
     },
     this.start = function () {
-        setInterval(this.count(), 1000);
+        intervalID = setInterval(this.count(), 1000);
     },
     this.stop= function () {
-        clearInterval(this.time);
+        clearInterval(this.intervalID);
     },
     this.isDone= function () {
         return (time === 0);
@@ -67,79 +86,138 @@ function Timer () {
     }
 }
 
-function Game() {
-    this.questions=questionArray,
-    this.answerTime=30,
-    this.currentQuestion=-1,
-    this.timer= new Timer(),
-    this.numCorrect=0,
-    this.setQuestion= function (index) {
-        //if index is within questions array, set question to questions[index]
-        if(index < this.questions.length) {
-            this.currentQuestion = index;
-            return true;
-        }
-        else return false; 
-    },
-    this.initGame= function () {
+/////////////////////////////////////////
+// GAME OBJECT 
+
+var game = {
+    isPlaying:false,
+    questions:questionArray,
+    answerTime:30,
+    currentQuestion:-1,
+    timer: new Timer("#gameTimer"),
+    numCorrect:0,
+    
+    
+    initGame: function () {
         this.answerTime = 30;
-        this.timer = new Timer();
+        this.timer = new Timer("#gameTimer");
         this.timer.set(30);
-        this.setQuestion(0);
+        this.isPlaying = true;
     },
-    this.displayCurrentQuestion= function () {
-        var curQuestion = this.questions[currentQuestion];
-        $(".question").text(curQuestion.question);
-        $(".answer1").text(curQuestion.answers[0]);
-        $(".answer2").text(curQuestion.answers[1]);
-        $(".answer3").text(curQuestion.answers[2]);
-        $(".answer4").text(curQuestion.answers[3]);
+    displayCurrentQuestion: function (display) {
+        if(display)
+        {  
+            var curQuestion = this.questions[this.currentQuestion];
+            $(".question").text(curQuestion.question);
+            $("#answer1").text(curQuestion.answers[0]);
+            $("#answer2").text(curQuestion.answers[1]);
+            $("#answer3").text(curQuestion.answers[2]);
+            $("#answer4").text(curQuestion.answers[3]);
+            $(".question-screen").show();
+        }
+        else{
+            $(".question-screen").hide();
+        }
     },
-    this.displayCurrentAnswer= function () {
-        var curQuestion = this.questions[currentQuestion];
-        $(".answer").text(curQuestion.answers[answerIndex]);
-        //display reward giphy
-        $(".reward").html(curQuestion.rewardSrc);
+    displayCurrentAnswer: function (display) {
+        if(display){
+            var curQuestion = this.questions[this.currentQuestion];
+            $(".answer").text(curQuestion.answers[curQuestion.answerIndex]);
+            //display reward giphy
+            $(".reward").html(curQuestion.rewardSrc);
+            $(".answer-screen").show();
+            //set a timeout to start the next question
+            var timeoutID = window.setTimeout(startNextQuestion, 3000);
+        }
+        else {
+            $(".answer-screen").hide();
+        }
     },
-    this.isAnswerCorrect= function (playerAnswer) {
-        var curQuestion = this.questions[currentQuestion];
+    questionEnded: function(answer){
+        this.timer.stop();
+        if (typeof answer == "undefined")//player didn't answer
+        {
+            //display no answer message
+        } 
+        else if (this.isAnswerCorrect(answer)) {
+            //correctly answered
+            this.numCorrect++;
+        }
+    },
+    isAnswerCorrect: function (playerAnswer) {
+        var curQuestion = this.questions[this.currentQuestion];
         if (playerAnswer == curQuestion.answerIndex)
             return true;
         else 
             return false;    
+    },
+    gameOver: function (){
+        this.isPlaying = false;
+        this.displayCurrentAnswer(false);
+        $("#start").text("Play Again");
+        $(".start-screen").show();
     }
 
 
 }
 
-
+function startNextQuestion ()
+{
+    var index = game.currentQuestion + 1;
+    if (index < game.questions.length){
+        game.currentQuestion = index;
+        game.displayCurrentQuestion(true);
+        game.displayCurrentAnswer(false);
+        game.timer.set(30);
+        game.timer.start();
+    }
+    else { //we reached the end of our question list
+        game.gameOver; 
+    }   
+}
 
 /////////////////////////////////////////////
 //  EVENT HANDLERS
 //
 $(document).ready(function () {
 
-if (currentGameState === gameState[0])
-{
-    $(".start-screen").show();
-    $(".question-screen").hide();
-    $(".answer-screen").hide();
-}
+    if (currentGameState === gameState[0])
+    {
+        $(".start-screen").show();
+        $(".question-screen").hide();
+        $(".answer-screen").hide();
+    }
 
-$("#start").on("click", function () {
-    console.log("Starting Game");
-    //start a new game
-    currentGameState = gameState[1];
-    
-    //create a new game and initialize the first question and answer set
-    var game = new Game;
-    game.initGame();
+    $("#start").on("click", function () {
+        console.log("Starting Game");
+        //change to question state
+        currentGameState = gameState[1];
+        
+        //create a new game and initialize the first question and answer set
+        game.initGame();
+        startNextQuestion();
 
-    //hide start and show question
-    $(".start-screen").hide();
-    $(".question-screen").show();
-});
+        //hide start and show question
+        $(".start-screen").hide();
+        game.displayCurrentQuestion(true);
+    }); //close start on click function
 
+
+    $(".answer-option").on("click", function () {
+        var userAnswer = $(this).attr("value");
+        console.log("Answer clicked");
+        //change to answer state
+        currentGameState = gameState[2];
+        
+        //notify game that question ended - game checks answer and stops timer
+        game.questionEnded(userAnswer);
+
+        //hide question
+        game.displayCurrentQuestion(false);
+        //show answer and move to next question after timeout
+        game.displayCurrentAnswer(true);
+
+    }); //close answer-option on click function
 
 
 
